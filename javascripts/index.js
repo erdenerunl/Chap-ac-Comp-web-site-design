2e79d67e1229b364292713207b90d2affa9ea1ea
erdenerunl <erdenerunale@gmail.com>
2020-11-03 20:07:10 +0300

    revisions and additions

---
+++ javascripts/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    items: 1
+  });
+
+});
+
+
--- scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#owl-carousel").owlCarousel();
-  });
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    items:1
-});
