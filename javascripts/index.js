91cacf987c4260fd888e471d831cc60932fcbc02
erdenerunl <erdenerunale@gmail.com>
2020-11-05 23:19:51 +0300

    carousel autoplay was added.

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   $('#owl-carousel').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplaySpeed: 1800,
+    autoplayHoverPause: true,
     loop: true,
     items: 1
   });
