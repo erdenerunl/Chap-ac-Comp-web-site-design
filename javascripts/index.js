432d34a10e38f51d815651a0c2f3cccd444d9cb7
erdenerunl <erdenerunale@gmail.com>
2020-11-09 18:40:52 +0300

    revision-1

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $('#owl-carousel').owlCarousel({
     autoplay: true,
-    autoplayTimeout: 5000,
+    autoplayTimeout: 8000,
     autoplaySpeed: 1800,
     autoplayHoverPause: true,
     loop: true,
@@ -9,5 +9,52 @@ $(document).ready(function () {
   });
 
 });
+$(document).ready(function () {
+  $('#owl-carousel-services').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplaySpeed: 1800,
+    autoplayHoverPause: true,
+    loop: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        768:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            autoplayTimeout: 4000,
+            autoplaySpeed: 1800,
+            autoplayHoverPause: true,
+            loop: true,
+            items:3,
+            nav:true,
+            loop:true
+        }
+    }
+  });
+
+});
+
+
+
+function toggleDropdown (e) {
+  const _d = $(e.target).closest('.dropdown'),
+    _m = $('.dropdown-menu', _d);
+  setTimeout(function(){
+    const shouldOpen = e.type !== 'click' && _d.is(':hover');
+    _m.toggleClass('show', shouldOpen);
+    _d.toggleClass('show', shouldOpen);
+    $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+  }, e.type === 'mouseleave' ? 300 : 0);
+}
+
+$('body')
+  .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+  .on('click', '.dropdown-menu a', toggleDropdown);
 
 
